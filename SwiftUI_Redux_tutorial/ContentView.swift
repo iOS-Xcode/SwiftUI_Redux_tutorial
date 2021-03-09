e25b3c471847676a9cddd34de8ed8c8f98078d71
Seokhyun Kim <wjapp72@gmail.com>
2021-03-09 09:38:06 -0800

    Initial Commit

---
+++ SwiftUI_Redux_tutorial/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_Redux_tutorial
+//
+//  Created by Seokhyun Kim on 2021-03-09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI_Redux_tutorial/SwiftUI_Redux_tutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Redux_tutorialApp.swift
+//  SwiftUI_Redux_tutorial
+//
+//  Created by Seokhyun Kim on 2021-03-09.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Redux_tutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
